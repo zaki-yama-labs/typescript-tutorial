05e121c3234a56db78d3c5e67008fd43de4dfabd
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-12-04 21:03:08 +0900

    Use interface

--- greeter.ts
+++ greeter.ts
@@ -1,7 +1,12 @@
-function greeter(person) {
-  return 'Hello, ' + person;
+interface Person {
+  firstName: string;
+  lastName: string;
 }
 
-let user = 'Jane User';
+function greeter(person: Person) {
+  return 'Hello, ' + person.firstName + ' ' + person.lastName;
+}
+
+let user: Person = { firstName: 'Jane', lastName: 'User' };
 
 document.body.innerHTML = greeter(user);
