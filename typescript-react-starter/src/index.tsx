9fcd367d6cd7ceb07c0cdd56b1ed689cee3c66e1
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-12-11 00:18:52 +0900

    Stateful Hello

--- typescript-react-starter/src/index.tsx
+++ typescript-react-starter/src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import Hello from './components/Hello';
+import Hello from './components/StatefulHello';
 
 ReactDOM.render(
   <Hello name="TypeScript" enthusiasmLevel={10} />,
