1a0fe92075c9d7d7edca40953d1233565a90e977
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-12-11 02:49:05 +0900

    Creating a store

--- typescript-react-starter/src/index.tsx
+++ typescript-react-starter/src/index.tsx
@@ -1,11 +1,24 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import Hello from './components/StatefulHello';
+import Hello from './containers/Hello';
+import { enthusiasm } from './reducers';
+import { StoreState } from './types';
+import { EnthusiasmAction } from './actions';
+
+const store = createStore<StoreState, EnthusiasmAction, any, any>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript',
+});
 
 ReactDOM.render(
-  <Hello name="TypeScript" enthusiasmLevel={10} />,
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
